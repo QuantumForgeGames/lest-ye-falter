37be17e99ab65d1c65f25050097f3a9ad14fbe37
Akshay <sakshays.2000@gmail.com>
2024-06-09 18:07:28 +0200

    fix infection bugs

--- scripts/cultist/dissent_state.gd
+++ scripts/cultist/dissent_state.gd
@@ -11,18 +11,21 @@ func enter() -> void:
 	
 	entity.change_sprite(Cultist.STATES.DISSENT)
 	entity.set_collision_layer_value(2, false) # exclude from AoI
-	_on_doubt_timer_timeout()
+	_on_dissent_timer_timeout()
 
 func exit() -> void:
-	entity.set_collision_layer_value(2, true) # include to AoI
-	entity.doubt_timer.stop()
+	entity.set_collision_layer_value(2, true) # include in AoI
+	entity.dissent_timer.stop()
 
 func on_hit() -> void:
 	entity.exit_scene()
 
-func _on_doubt_timer_timeout() -> void:
+func _on_dissent_timer_timeout() -> void:
+	entity.dissent_timer.start(randf_range(DELAY_MIN, DELAY_MAX))
+	
 	var neighbours = entity.area_of_influence.get_overlapping_bodies()
+	if neighbours.size() == 0: return # if no infectable neighbours, do nothing
+	
+	# alter probability logic to take into account the number of neighbours
 	if randf() < CONVERSION_CHANCE:
 		neighbours.pick_random().set_state(Cultist.STATES.DOUBT)
-	
-	entity.doubt_timer.start(randf_range(DELAY_MIN, DELAY_MAX))
--- scripts/cultist/doubt_state.gd
+++ scripts/cultist/doubt_state.gd
@@ -1,14 +1,28 @@
 extends State
 
+@export var CONVERSION_CHANCE: float = 0.1
+
 func enter() -> void:
 	var tween := get_tree().create_tween()
 	tween.tween_property(entity, "scale", Vector2(1.5, 1.5), 0.25)
 	tween.tween_property(entity, "scale", Vector2(1., 1.), 0.25)
 	
 	entity.change_sprite(Cultist.STATES.DOUBT)
+	entity.set_collision_layer_value(2, false) # exclude from AoI
+	entity.doubt_timer.start()
+
+func exit() -> void:
+	entity.set_collision_layer_value(2, true) # include in AoI
+	entity.doubt_timer.stop()
 
 func process(_delta: float) -> void:	
 	pass
  
 func on_hit() -> void:
 	entity.exit_scene()
+
+func _on_doubt_timer_timeout() -> void:
+	if randf() < CONVERSION_CHANCE:
+		transitioned.emit("Dissent")
+	else:
+		entity.doubt_timer.start()
