5fbdebd8341b54224f8ac48d20743ed07b83ff82
Akshay <sakshays.2000@gmail.com>
2024-06-12 21:28:52 +0200

    edit pausing

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -11,13 +11,13 @@ func _on_level_lost():
 	game_over_container.show()
 	var tween := get_tree().create_tween()
 	tween.tween_property(game_over_container, "modulate:a", 1., 1.)
-	tween.tween_callback(func(): get_tree().root.get_child(-1).get_node("Paddle").process_mode = Node.PROCESS_MODE_DISABLED)
+	tween.tween_callback(func(): get_tree().root.get_child(-1).process_mode = Node.PROCESS_MODE_DISABLED)
 	
 func _on_level_won():
 	level_complete_container.show()
 	var tween := get_tree().create_tween()
 	tween.tween_property(level_complete_container, "modulate:a", 1., 1.)
-	tween.tween_callback(func(): get_tree().root.get_child(-1).get_node("Paddle").process_mode = Node.PROCESS_MODE_DISABLED)
+	tween.tween_callback(func(): get_tree().root.get_child(-1).process_mode = Node.PROCESS_MODE_DISABLED)
 
 # BUTTONS
 func _on_restart_button_pressed():
