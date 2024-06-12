6cf6febb1201b20e57aab5d396722684c3d036f8
Akshay <sakshays.2000@gmail.com>
2024-06-12 20:08:09 +0200

    track number of cultist types throughout game

--- scripts/globals/event_manager.gd
+++ scripts/globals/event_manager.gd
@@ -4,3 +4,5 @@ signal cultist_captured(cultist: Cultist)
 signal cultist_convinced(cultist: Cultist)
 signal cultist_killed(cultist: Cultist)
 signal cultist_escaped(cultist: Cultist)
+
+signal cultist_state_changed(cultist: Cultist)
