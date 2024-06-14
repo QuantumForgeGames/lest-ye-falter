5c2bb082877b4a7271e818e1c64192c521441335
Akshay <sakshays.2000@gmail.com>
2024-06-14 22:01:28 +0200

    minigame ui runs faster

--- scripts/system/minigame_manager.gd
+++ scripts/system/minigame_manager.gd
@@ -48,10 +48,10 @@ func _on_cultist_captured(cultist: Cultist):
 	
 	# visuals
 	tween = get_tree().create_tween()
-	tween.tween_property(minigame_ui, "modulate:a", 1., 0.5)
+	tween.tween_property(minigame_ui, "modulate:a", 1., 0.25)
 	
 	for glyph in minigame_ui.get_node("Glyphs").get_children():
-		tween.tween_property(glyph, "modulate:a", 1., 0.2)
+		tween.tween_property(glyph, "modulate:a", 1., 0.1)
 		
 	tween.tween_callback(_start_minigame)
 
