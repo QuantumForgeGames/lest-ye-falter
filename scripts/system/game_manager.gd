d77bb9109b0d49d49860432d3e16477f73bfb3e7
qaptoR <rocco.ruscitti@outlook.com>
2024-06-14 18:52:07 -0700

    updated arms paddle collider to be closer to visuals

--- scripts/system/game_manager.gd
+++ scripts/system/game_manager.gd
@@ -25,73 +25,73 @@ var num_dissent: int = 0
 var level_active: bool = false
 
 func _ready() -> void:
-	print(get_parent().name)
-	ball.set_max_hits(MAX_HITS_PER_PADDLE_BOUNCE)
-	LevelPrompt._hide_panels()
-	
-	EventManager.cultist_convinced.connect(_on_cultist_convinced)
-	EventManager.cultist_killed.connect(_on_cultist_killed)
-	EventManager.cultist_escaped.connect(_on_cultist_escaped)
-	
-	EventManager.cultist_state_changed.connect(_recompute_stats)
-	EventManager.cultist_killed.connect(_recompute_stats)
-	EventManager.cultist_escaped.connect(_recompute_stats)
-	get_tree().create_timer(5).timeout.connect(func(): level_active = true)
-	
+    print(get_parent().name)
+    ball.set_max_hits(MAX_HITS_PER_PADDLE_BOUNCE)
+    LevelPrompt._hide_panels()
+    
+    EventManager.cultist_convinced.connect(_on_cultist_convinced)
+    EventManager.cultist_killed.connect(_on_cultist_killed)
+    EventManager.cultist_escaped.connect(_on_cultist_escaped)
+    
+    EventManager.cultist_state_changed.connect(_recompute_stats)
+    EventManager.cultist_killed.connect(_recompute_stats)
+    EventManager.cultist_escaped.connect(_recompute_stats)
+    get_tree().create_timer(5).timeout.connect(func(): level_active = true)
+    
 func _on_cultist_convinced(cultist: Cultist) -> void:
-	cultist.return_to_spawn()
-	num_converted += 1
+    cultist.return_to_spawn()
+    num_converted += 1
 
 func _on_cultist_killed(cultist: Cultist) -> void:
-	match cultist.get_current_state():
-		"Base":
-			num_base_killed += 1
-		"Doubt":
-			num_doubt_killed += 1
-		"Dissent":
-			num_dissent_killed += 1
-		"Server":
-			num_doubt_killed += 1
-			
-	cultist.exit_scene()
-	cultist.reparent(get_parent())
+    match cultist.get_current_state():
+        "Base":
+            num_base_killed += 1
+        "Doubt":
+            num_doubt_killed += 1
+        "Dissent":
+            num_dissent_killed += 1
+        "Server":
+            num_doubt_killed += 1
+            
+    cultist.exit_scene()
+    cultist.reparent(get_parent())
 
 func _on_cultist_escaped(cultist: Cultist) -> void:
-	num_escaped += 1
-	cultist.reparent(get_parent())
+    num_escaped += 1
+    cultist.reparent(get_parent())
 
 func _recompute_stats(_cultist: Cultist) -> void:
-	num_base = 0
-	num_doubt = 0
-	num_dissent = 0
+    num_base = 0
+    num_doubt = 0
+    num_dissent = 0
 
-	for cultist in cultist_spawner.get_children():
-		match cultist.get_current_state():
-			"Base":
-				num_base += 1
-			"Doubt":
-				num_doubt += 1
-			"Dissent":
-				num_dissent += 1
-			"Server":
-				num_doubt += 1
-	
-	if level_active: _check_game_status()
+    for cultist in cultist_spawner.get_children():
+        match cultist.get_current_state():
+            "Base":
+                num_base += 1
+            "Doubt":
+                num_doubt += 1
+            "Dissent":
+                num_dissent += 1
+            "Server":
+                num_doubt += 1
+    
+    if level_active: _check_game_status()
 
 func _check_game_status():
-	var doubt = float(num_base)/(num_base + num_doubt + num_dissent)
-	_tween_doubt_shader(doubt)
-	if doubt < 0.15:
-		EventManager.level_lost.emit()
-	if num_dissent == 0:
-		EventManager.level_won.emit()
+    var doubt = float(num_base)/(num_base + num_doubt + num_dissent)
+    _tween_doubt_shader(doubt)
+    if doubt < 0.15:
+        EventManager.level_lost.emit()
+    if num_dissent == 0:
+        EventManager.level_won.emit()
 
 func _tween_doubt_shader (doubt :float) -> void:
-	var cur_doubt = doubt_shader.material.get_shader_parameter("transparency")
-	var tween = self.create_tween()
-	tween.tween_method(func(d):
-		doubt_shader.material.set_shader_parameter("transparency", d)
-		doubt_shader.material.set_shader_parameter("thresholds", PackedFloat32Array(
-			[0.6, 0.67, 0.75].map(func(x): return clampf(0.0, x, x *d *2))
-		))
-	, cur_doubt, 1. -doubt, 0.25)
+    var cur_doubt = doubt_shader.material.get_shader_parameter("transparency")
+    var tween = self.create_tween()
+    tween.tween_method(func(d):
+        doubt_shader.material.set_shader_parameter("transparency", d)
+        doubt_shader.material.set_shader_parameter("thresholds", PackedFloat32Array(
+            [0.6, 0.67, 0.75].map(func(x): return clampf(0.0, x, x *d *2))
+        ))
+    , cur_doubt, 1. -doubt, 0.25)
